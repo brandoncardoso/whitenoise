a13018cd8ba4ab5d6b706c3e8fe9e5093bf5cbe3
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2020-11-04 16:34:39 -0500

    inc/dec timer dialog values with buttons

--- app/src/main/java/com/bcardoso/whitenoise/ui/main/SoundControlFragment.kt
+++ app/src/main/java/com/bcardoso/whitenoise/ui/main/SoundControlFragment.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.fragment.app.Fragment
@@ -79,23 +79,59 @@ class SoundControlFragment : Fragment() {
 
     private fun openSetTimeDialog(view: View) {
         val dialogBuilder = AlertDialog.Builder(view.context)
-        val inflater = requireActivity().layoutInflater;
+        val dialogView = requireActivity().layoutInflater.inflate(R.layout.timer_dialog, null)
+
+        var curHour = 0
+        var curMin = 0
+        val hourMinMax = Pair(0, 23)
+        val hourIncrement = 1
+        val minuteIncrement = 5
+
+        val hourText = dialogView.findViewById<TextView>(R.id.tv_hour)
+        dialogView.findViewById<ImageButton>(R.id.btn_hour_increase)?.setOnClickListener {
+            curHour += hourIncrement
+            curHour = curHour.coerceAtMost(hourMinMax.second)
+            hourText?.text = curHour.toString()
+        }
+        dialogView.findViewById<ImageButton>(R.id.btn_hour_decrease)?.setOnClickListener {
+            curHour -= hourIncrement
+            curHour = curHour.coerceAtLeast(hourMinMax.first)
+            hourText?.text = curHour.toString()
+        }
+        val minuteText = dialogView.findViewById<TextView>(R.id.tv_minute)
+        dialogView.findViewById<ImageButton>(R.id.btn_minute_increase)?.setOnClickListener {
+            curMin += minuteIncrement
+            if (curMin >= 60) {
+                curMin %= 60
+                curHour = (curHour + hourIncrement).coerceAtMost(hourMinMax.second)
+                hourText?.text = curHour.toString()
+            }
+            minuteText?.text = curMin.toString()
+        }
+        dialogView.findViewById<ImageButton>(R.id.btn_minute_decrease)?.setOnClickListener {
+            curMin -= minuteIncrement
+            if (curMin < 0 ) {
+                curMin += 60
+                curHour = (curHour - hourIncrement).coerceAtLeast(hourMinMax.first)
+                hourText?.text = curHour.toString()
+            }
+            minuteText?.text = curMin.toString()
+        }
 
         dialogBuilder
-            .setView(inflater.inflate(R.layout.timer_dialog, null))
+            .setView(dialogView)
             .setCancelable(true)
-            .setPositiveButton("Proceed", ::setTimerFromDialog)
+            .setPositiveButton("Set", ::setTimerFromDialog)
             .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
-
-        val alert = dialogBuilder.create()
-        alert.setTitle("Stop sounds in...")
-        alert.show()
+            .setTitle("Stop sounds in...")
+            .create()
+            .show()
     }
 
     private fun setTimerFromDialog(di: DialogInterface, id: Int) {
         val dialog = di as AlertDialog
-        val hours = Integer.parseInt(dialog.findViewById<EditText>(R.id.et_hour)?.text.toString())
-        val minutes = Integer.parseInt(dialog.findViewById<EditText>(R.id.et_minute)?.text.toString())
+        val hours = Integer.parseInt(dialog.findViewById<TextView>(R.id.tv_hour)?.text.toString())
+        val minutes = Integer.parseInt(dialog.findViewById<TextView>(R.id.tv_minute)?.text.toString())
 
         val totalMillis = ((hours * 60 * 60000) + (minutes * 60000)).toLong()
         setTimer(totalMillis)
