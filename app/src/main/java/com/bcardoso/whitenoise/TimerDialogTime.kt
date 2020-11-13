57ce50860e413ea7f2bfcea7f6bbe1c76e1f7ca7
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2020-11-12 21:51:26 -0500

    move TimerDialogTime to separate file

---
+++ app/src/main/java/com/bcardoso/whitenoise/TimerDialogTime.kt
@@ -0,0 +1,50 @@
+package com.bcardoso.whitenoise
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class TimerDialogTime(hoursInitial: Int, minutesInitial: Int) {
+    private var _hours: MutableLiveData<Int> = MutableLiveData(hoursInitial)
+    private var _minutes: MutableLiveData<Int> = MutableLiveData(minutesInitial)
+    val hours: LiveData<Int> = _hours
+    val minutes: LiveData<Int> = _minutes
+
+    private val hoursIncrement = 1
+    private val minutesIncrement = 5
+
+    private fun increment(x: Int, increment: Int, max: Int): Int {
+        var retVal = x + increment
+        if (retVal >= max) {
+            retVal %= max
+        }
+        return retVal
+    }
+
+    private fun decrement(x: Int, decrement: Int, max: Int, min: Int): Int {
+        var retVal = x - decrement
+        if (retVal < min) {
+            retVal += max
+        }
+        return retVal
+    }
+
+    fun incrementHours() {
+        _hours.value = increment(_hours.value!!, hoursIncrement, 24)
+    }
+
+    fun decrementHours() {
+        _hours.value = decrement(_hours.value!!, hoursIncrement, 24, 0)
+    }
+
+    fun incrementMinutes() {
+        _minutes.value = increment(_minutes.value!!, minutesIncrement, 60)
+    }
+
+    fun decrementMinutes() {
+        _minutes.value = decrement(_minutes.value!!, minutesIncrement, 60, 0)
+    }
+
+    fun getMillis(): Long {
+        return (((_hours.value!! * 60) + _minutes.value!!) * 60 * 1000).toLong()
+    }
+}
--- app/src/main/java/com/bcardoso/whitenoise/ui/main/SoundControlFragment.kt
+++ app/src/main/java/com/bcardoso/whitenoise/ui/main/SoundControlFragment.kt
@@ -10,63 +10,15 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bcardoso.whitenoise.ActiveSoundAdapter
 import com.bcardoso.whitenoise.R
 import com.bcardoso.whitenoise.SoundControlInterface
 import com.bcardoso.whitenoise.databinding.TimerDialogBinding
+import com.bcardoso.whitenoise.TimerDialogTime
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.concurrent.TimeUnit
-
-class TimerDialogTime(hoursInitial: Int, minutesInitial: Int) {
-
-    private var _hours: MutableLiveData<Int> = MutableLiveData(hoursInitial)
-    private var _minutes: MutableLiveData<Int> = MutableLiveData(minutesInitial)
-    val hours: LiveData<Int> = _hours
-    val minutes: LiveData<Int> = _minutes
-
-    private val hoursIncrement = 1
-    private val minutesIncrement = 5
-
-    private fun increment(x: Int, increment: Int, max: Int): Int {
-        var retVal = x + increment
-        if (retVal >= max) {
-            retVal %= max
-        }
-        return retVal
-    }
-
-    private fun decrement(x: Int, decrement: Int, max: Int, min: Int): Int {
-        var retVal = x - decrement
-        if (retVal < min) {
-            retVal += max
-        }
-        return retVal
-    }
-
-    fun incrementHours() {
-        _hours.value = increment(_hours.value!!, hoursIncrement, 24)
-    }
-
-    fun decrementHours() {
-        _hours.value = decrement(_hours.value!!, hoursIncrement, 24, 0)
-    }
-
-    fun incrementMinutes() {
-        _minutes.value = increment(_minutes.value!!, minutesIncrement, 60)
-    }
-
-    fun decrementMinutes() {
-        _minutes.value = decrement(_minutes.value!!, minutesIncrement, 60, 0)
-    }
-
-    fun getMillis(): Long {
-        return (((_hours.value!! * 60) + _minutes.value!!) * 60 * 1000).toLong()
-    }
-}
 
 class SoundControlFragment : Fragment() {
     private lateinit var mContext: Context
