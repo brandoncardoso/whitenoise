8eca628871dad73d1ce88a189d93755bdb72bc3d
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2020-11-25 17:31:03 -0500

    rename about fragment class

--- app/src/main/java/com/bcardoso/whitenoise/fragments/AboutFragment.kt
+++ app/src/main/java/com/bcardoso/whitenoise/fragments/AboutFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bcardoso.whitenoise.R
 
-class About : Fragment() {
+class AboutFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
